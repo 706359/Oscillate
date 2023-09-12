c4e956fb88191056b95725b9dd449deb656aab7f
Shiv Ram <706359@gmail.com>
2023-09-12 11:59:41 -0500

    updated

--- main.js
+++ main.js
@@ -33,3 +33,20 @@ const slider = new A11YSlider(document.querySelector(".slider"), {
 		},
 	},
 });
+
+let imgBx = document.querySelectorAll(".imgBx");
+let contentBx = document.querySelectorAll(".contentBx");
+
+for (let i = 0; i < imgBx.length; i++) {
+	imgBx[i].addEventListener("mouseover", function () {
+		for (let i = 0; i < contentBx.length; i++) {
+			contentBx[i].className = "contentBx";
+		}
+		document.getElementById(this.dataset.id).className = "contentBx active";
+
+		for (let i = 0; i < imgBx.length; i++) {
+			imgBx[i].className = "imgBx";
+		}
+		this.className = "imgBx active";
+	});
+}
